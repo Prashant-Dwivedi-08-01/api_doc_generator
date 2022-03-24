f503aa47b3371b27044702f7edc01381e66a0e00
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-03-24 14:32:32 +0530

    Project Completed

--- ui/src/components/Home/home.js
+++ ui/src/components/Home/home.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { uploadFile } from "../../actions/home"
 import { useDispatch } from "react-redux";
 import { useSelector } from 'react-redux';
+import "./home.css"
 
 const Home = () => {
 
@@ -10,7 +11,7 @@ const Home = () => {
 
     const html_doc = useSelector(state => state.html)
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData()
         formData.append('collection', uploadedFile.current.files[0])
@@ -19,7 +20,7 @@ const Home = () => {
             method: 'POST',
             body: formData
         };
-        
+
         fetch('http://localhost:5000/upload', requestOptions)
             .then(response => {
                 response.blob().then(blob => {
@@ -29,19 +30,41 @@ const Home = () => {
                     a.download = 'doc.html';
                     a.click();
                 });
-        });
-    } 
+            });
+    }
 
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            {/* <form onSubmit={handleSubmit}>
                 <label>
                     File:
                     <input type="file" ref={uploadedFile} />
                 </label>
                 <input type="submit" value="Submit" />
-            </form>
+            </form> */}
+            <div class="v684_4074"><h1 class="v712_4145">API COLLECTION</h1><h1 class="v802_4026">TO</h1><h1
+                class="v712_4146">HTML DOC</h1>
+                <div class="v712_4147">
+                    <div class="v714_4082"></div>
+                    <div class="v714_4080"></div>
+                    <div class="v712_4150">
+                        <div class="v712_4151"></div>
+                        <div class="v712_4152"></div>
+                    </div>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    {/* <label>
+                        File:
+                        <input type="file" ref={uploadedFile} />
+                    </label>
+                    <input type="submit" value="Submit" /> */}
+                <input type="file" className="file-input" ref={uploadedFile} />
+                <div class="v802_4027"></div>
+                <span class="v802_4028" onClick={handleSubmit} style={{"cursor":"pointer"}}>Upload</span>
+                <span class="v802_4029" >Upload your Postman collection here</span>
+                </form> 
+            </div>
         </>
     )
 }
